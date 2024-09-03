8afda000b35a324514916958cd16b9e77068e839
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-09-03 17:49:58 +0700

    pertemuan1

---
+++ Pertemuan1/src/main/java/Latihan2/DemoNilai.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Latihan2;
+
+/**
+ *
+ * @author Irzi Rhmtllh
+ */
+public class DemoNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Nilai myNilai = new Nilai("23092", "Irzi Rahmatullah", 90, 92, 95, 90);
+        myNilai.CetakNilai();
+    }
+    
+}
